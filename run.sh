6b4b2e19eea86e40a8c54dabd10d3c4bb169d78d
shazz <604708+shazz@users.noreply.github.com>
2025-01-01 18:43:29 -0500

    Add files via upload

--- run.sh
+++ run.sh
@@ -2,15 +2,19 @@ echo Building floppy disk
 mkdir -p build/ramtos
 mkdir -p build/ramtos/AUTO
 cp TOS/TOS206FR.IMG build/ramtos/RTOS.IMG
-cp build/RAMTOS8B.PRG build/ramtos/AUTO/
+cp build/RAMTOS8B.TOS build/ramtos/AUTO/RAMTOS8B.PRG
 tools/dir2msa build/ramtos
 tools/msa-to-st-converter build/ build/
 
 rm -rf bin/*
 mkdir -p bin
 cp -r TOS bin/
+
+echo Copy a few TOS images in the root dir
 cp TOS/TOS206FR.IMG bin/RTOS.IMG
 cp TOS/TOS206SW.IMG bin/
+cp TOS/TOS206X.IMG bin/
+
 cp build/*.TOS bin/
 cp -r tests/* bin/
 cd /d/projects/stvirus/tools/hatari 
