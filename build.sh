70424412d0670ae0ea127d8b3a74a02dcd9ef042
shazz <604708+shazz@users.noreply.github.com>
2025-01-01 18:16:50 -0500

    Add files via upload

--- build.sh
+++ build.sh
@@ -5,16 +5,16 @@ mkdir -p TOS
 mkdir -p tmp
 mkdir -p bin
 rm -rf bin/*
-
+rm -rf build/*
 
 echo Building RAMTOS8B
-tools/vasmm68k_mot -devpac -spaces -nosym -Ftos -D_RAMTOS_=1 -D_SELTOS_=1 src/RAMTOS8.S -o build/RAMTOS8B.PRG -L build/RAMTOS8B.LS
+tools/vasmm68k_mot -devpac -spaces -nosym -Ftos -D_RAMTOS_=1 -D_SELTOS_=1 src/RAMTOS8.S -o build/RAMTOS8B.TOS -L build/RAMTOS8B.LS
 echo Done!
 
 echo Building RAMTOS8
-tools/vasmm68k_mot -devpac -spaces -nosym -Ftos -D_RAMTOS_=0 -D_SELTOS_=1 src/RAMTOS8.S -o build/RAMTOS8.PRG -L build/RAMTOS8.LS
+tools/vasmm68k_mot -devpac -spaces -nosym -Ftos -D_RAMTOS_=0 -D_SELTOS_=1 src/RAMTOS8.S -o build/RAMTOS8.TOS -L build/RAMTOS8.LS
 echo Done!
 
 echo Building SELTO8
-tools/vasmm68k_mot -devpac -spaces -nosym -Ftos -D_RAMTOS_=0 -D_SELTOS_=0 src/RAMTOS8.S -o build/SELTOS8.PRG -L build/SELTOS8.LS
+tools/vasmm68k_mot -devpac -spaces -nosym -Ftos -D_RAMTOS_=0 -D_SELTOS_=0 src/RAMTOS8.S -o build/SELTOS8.TOS -L build/SELTOS8.LS
 echo Done!
--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ mkdir -p bin
 cp -r TOS bin/
 cp TOS/TOS206FR.IMG bin/RTOS.IMG
 cp TOS/TOS206SW.IMG bin/
-cp build/*.PRG bin/
+cp build/*.TOS bin/
 cp -r tests/* bin/
 cd /d/projects/stvirus/tools/hatari 
 ./hatari.exe --machine STE -s 4 --tos "TOS\TOS_v1.62_fr.img" "D:\projects\ACSI2ST\UTILITY\SELTOS\BIN" 
