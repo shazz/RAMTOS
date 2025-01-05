09a48386f4fd6a0e11cc7968e4f5ee4af6921b58
shazz <604708+shazz@users.noreply.github.com>
2025-01-04 21:15:30 -0500

    Add files via upload

--- gen_floppy.sh
+++ gen_floppy.sh
@@ -6,5 +6,10 @@ mkdir -p build/ramtos
 mkdir -p build/ramtos/AUTO
 cp TOS/TOS206FR.IMG build/ramtos/RTOS.IMG
 cp build/RAMTOS8B.TOS build/ramtos/AUTO/RAMTOS8B.PRG
+cp -r tests/SYSINFO build/ramtos/
+
+echo Building MSA image
 tools/dir2msa build/ramtos
+
+echo Building ST image
 tools/msa-to-st-converter build/ build/
